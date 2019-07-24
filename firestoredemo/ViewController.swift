da6f7984a27b83645534c9a865df806e27eda308
Eliezer Sant Ana <eliezer.santana@t-systems.com.br>
2019-07-24 20:14:32 -0300

    Initial Commit

---
+++ firestoredemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firestoredemo
+//
+//  Created by TMAN on 24/07/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
